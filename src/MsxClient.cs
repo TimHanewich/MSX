b797a4079793cd0c6d0390ebabc8837bf39aa066
Tim Hanewich <tahanewich@live.com>
2026-03-27 08:02:19 -0400

    First commit after initial development

---
+++ src/MsxClient.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MSX
+{
+    public class MsxClient
+    {
+        private const string URL_ROOT = "https://microsoftsales.crm.dynamics.com/api/data/v9.2/";
+
+        private readonly string _cookie;
+
+        public MsxClient(string cookie)
+        {
+            if (string.IsNullOrWhiteSpace(cookie))
+                throw new Exception("Cookie cannot be blank!");
+            _cookie = cookie;
+        }
+
+        private HttpRequestMessage PrepareRequest()
+        {
+            var req = new HttpRequestMessage();
+            req.Headers.Add("cookie", _cookie);
+            return req;
+        }
+
+        private async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var req = PrepareRequest();
+            req.RequestUri = new Uri(url);
+            var hc = new HttpClient();
+            return await hc.SendAsync(req);
+        }
+
+        private async Task<HttpResponseMessage> PostAsync(string url, JObject body)
+        {
+            var req = PrepareRequest();
+            req.Method = HttpMethod.Post;
+            req.RequestUri = new Uri(url);
+            req.Content = new StringContent(body.ToString(), Encoding.UTF8, "application/json");
+            var hc = new HttpClient();
+            return await hc.SendAsync(req);
+        }
+
+        public async Task<JArray> RunQueryAsync(string odataQuery)
+        {
+            string url = URL_ROOT + odataQuery;
+            var resp = await GetAsync(url);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception($"OData query '{odataQuery}' returned {resp.StatusCode}. Msg: {content}");
+
+            JObject root = JObject.Parse(content);
+            JProperty? prop = root.Property("value");
+            if (prop != null)
+                return (JArray)prop.Value;
+
+            throw new Exception($"Unable to find 'value' property in OData response to query '{odataQuery}'");
+        }
+
+        public async Task<string> WhoAmIAsync()
+        {
+            string url = URL_ROOT + "WhoAmI()";
+            var resp = await GetAsync(url);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception($"WhoAmI returned {resp.StatusCode}! Msg: {content}");
+
+            JObject jo = JObject.Parse(content);
+            JProperty? prop = jo.Property("UserId");
+            if (prop != null)
+                return prop.Value.ToString();
+
+            throw new Exception("Unable to find UserId in WhoAmI response.");
+        }
+
+        public async Task<JArray> SearchUsersAsync(string fullname)
+        {
+            string query = $"systemusers?$select=msp_rolesummary,msp_solutionarea,title,msp_qualifier2,internalemailaddress,msp_salesdistrictname,fullname,msp_solutionareadetails&$filter=contains(fullname,'{fullname}')";
+            return await RunQueryAsync(query);
+        }
+
+        public async Task<JArray> SearchAccountsAsync(string searchTerm)
+        {
+            string url = URL_ROOT + $"accounts?$filter=contains(name, '{searchTerm}')";
+            var resp = await GetAsync(url);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception($"Searching accounts returned {resp.StatusCode}! Msg: {content}");
+
+            JObject jo = JObject.Parse(content);
+            JArray source = (JArray)jo["value"]!;
+            JArray result = new JArray();
+            foreach (JObject account in source)
+            {
+                result.Add(new JObject
+                {
+                    ["name"] = account["name"],
+                    ["accountid"] = account["accountid"]
+                });
+            }
+            return result;
+        }
+
+        public async Task<JArray> SearchOpportunitiesAsync(string accountId, string titleSearch)
+        {
+            string url = URL_ROOT + $"opportunities?$filter=_parentaccountid_value eq '{accountId}' and statecode eq 0 and contains(name, '{titleSearch}')";
+            var resp = await GetAsync(url);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception($"Searching opportunities returned {resp.StatusCode}! Msg: {content}");
+
+            JObject jo = JObject.Parse(content);
+            JArray source = (JArray)jo["value"]!;
+            JArray result = new JArray();
+            foreach (JObject opp in source)
+            {
+                result.Add(new JObject
+                {
+                    ["opportunityid"] = opp["opportunityid"],
+                    ["name"] = opp["name"],
+                    ["description"] = opp["description"],
+                    ["estimatedvalue"] = opp["estimatedvalue"]
+                });
+            }
+            return result;
+        }
+
+        public async Task CreateTaskAsync(string title, string description, DateTime timestamp, string? accountId = null, string? opportunityId = null)
+        {
+            JObject body = new JObject();
+            body.Add("subject", title);
+            body.Add("description", description);
+            body.Add("scheduledstart", timestamp.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+            if (accountId != null)
+                body.Add("regardingobjectid_account@odata.bind", $"/accounts({accountId})");
+            else if (opportunityId != null)
+                body.Add("regardingobjectid_opportunity@odata.bind", $"/opportunities({opportunityId})");
+
+            var resp = await PostAsync(URL_ROOT + "tasks", body);
+            if (resp.StatusCode != System.Net.HttpStatusCode.NoContent)
+            {
+                string content = await resp.Content.ReadAsStringAsync();
+                throw new Exception($"Creation of task returned {resp.StatusCode}. Msg: {content}");
+            }
+        }
+
+        public async Task<JArray> GetMyRecentTasksAsync()
+        {
+            string userId = await WhoAmIAsync();
+            string url = URL_ROOT + $"tasks?$filter= _ownerid_value eq '{userId}'&$top=150&$orderby=scheduledstart desc&$expand=regardingobjectid_account($select=name,accountid),regardingobjectid_opportunity($select=name,opportunityid)";
+
+            var resp = await GetAsync(url);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception($"Getting recent tasks returned {resp.StatusCode}! Msg: {content}");
+
+            JObject root = JObject.Parse(content);
+            JArray tasks = (JArray)root["value"]!;
+            JArray result = new JArray();
+
+            foreach (JObject task in tasks)
+            {
+                JObject summary = new JObject
+                {
+                    ["subject"] = task["subject"],
+                    ["description"] = task["description"],
+                    ["scheduledstart"] = task["scheduledstart"]
+                };
+
+                JObject? account = task["regardingobjectid_account"] as JObject;
+                JObject? opportunity = task["regardingobjectid_opportunity"] as JObject;
+
+                if (account != null && account.HasValues)
+                {
+                    summary["regarding"] = new JObject
+                    {
+                        ["type"] = "account",
+                        ["name"] = account["name"],
+                        ["id"] = account["accountid"]
+                    };
+                }
+                else if (opportunity != null && opportunity.HasValues)
+                {
+                    summary["regarding"] = new JObject
+                    {
+                        ["type"] = "opportunity",
+                        ["name"] = opportunity["name"],
+                        ["id"] = opportunity["opportunityid"]
+                    };
+                }
+
+                result.Add(summary);
+            }
+
+            return result;
+        }
+
+        public async Task<JArray> GetAssociatedOpportunitiesAsync(string systemUserId)
+        {
+            string fetchxml = @"<fetch version=""1.0"" output-format=""xml-platform"" mapping=""logical"" distinct=""true"" no-lock=""false""><entity name=""opportunity""><attribute name=""transactioncurrencyid""/><attribute name=""msp_parentopportunityid""/><attribute name=""name""/><attribute name=""description""/><attribute name=""estimatedvalue""/><attribute name=""estimatedclosedate""/><attribute name=""ownerid""/><attribute name=""createdon""/><attribute name=""parentcontactid""/><attribute name=""parentaccountid""/><attribute name=""msp_partneraccountid""/><attribute name=""msp_recommendationcode""/><attribute name=""msp_licensingprogram""/><attribute name=""msp_opportunitynumber""/><attribute name=""msp_activesalesstage""/><attribute name=""msp_solutionarea""/><attribute name=""msp_salesplay""/><attribute name=""statuscode""/><attribute name=""statecode""/><attribute name=""msp_opportunitytype""/><attribute name=""msp_estcompletiondate""/><attribute name=""msp_engagementstatus""/><attribute name=""msp_consumptionactivedevicesrecurring""/><attribute name=""msp_consumptionactivedevicesnonrecurring""/><attribute name=""msp_consumptionconsumedrecurring""/><attribute name=""msp_consumptionconsumednonrecurring""/><attribute name=""opportunityid""/><attribute name=""msp_riskscore""/><filter type=""and""><condition attribute=""statecode"" operator=""eq"" value=""0""/></filter><link-entity name=""team"" to=""opportunityid"" from=""regardingobjectid"" alias=""aa"" link-type=""inner""><filter type=""and""><condition attribute=""teamtype"" operator=""eq"" value=""1""/><condition attribute=""name"" operator=""like"" value=""%cc923a9d-7651-e311-9405-00155db3ba1e""/></filter><link-entity name=""teammembership"" intersect=""true"" visible=""false"" from=""teamid"" to=""teamid""><link-entity name=""systemuser"" alias=""ab"" from=""systemuserid"" to=""systemuserid""><filter type=""and""><condition attribute=""systemuserid"" operator=""eq"" value=""{PLACEHOLDER_USER_ID}""/></filter></link-entity></link-entity></link-entity><link-entity name=""account"" visible=""false"" to=""parentaccountid"" from=""accountid"" link-type=""outer"" alias=""a_76946cd0245c4349bbb98a1ed211155a""><attribute name=""territoryid""/><attribute name=""address1_country""/><attribute name=""name""/><attribute name=""accountid""/></link-entity><order attribute=""parentaccountid"" descending=""false""/></entity></fetch>";
+
+            fetchxml = fetchxml.Replace("PLACEHOLDER_USER_ID", systemUserId);
+            string query = "opportunities?fetchXml=" + Uri.EscapeDataString(fetchxml);
+
+            JArray data = await RunQueryAsync(query);
+
+            string alias = "a_76946cd0245c4349bbb98a1ed211155a";
+            JArray result = new JArray();
+            foreach (JObject opp in data)
+            {
+                JObject summary = new JObject
+                {
+                    ["opportunityid"] = opp["opportunityid"],
+                    ["name"] = opp["name"],
+                    ["description"] = opp["description"],
+                    ["value"] = opp["estimatedvalue"],
+                    ["closeDate"] = opp["estimatedclosedate"]
+                };
+
+                summary["account"] = new JObject
+                {
+                    ["id"] = opp[$"{alias}.accountid"],
+                    ["name"] = opp[$"{alias}.name"]
+                };
+
+                result.Add(summary);
+            }
+
+            return result;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,293 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MSX
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly string StorageDir = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MSX");
+        private static readonly string CookiePath = Path.Combine(StorageDir, "cookie.txt");
+
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return 1;
+            }
+
+            string command = args[0].ToLowerInvariant();
+
+            try
+            {
+                switch (command)
+                {
+                    case "auth":
+                        return HandleAuth(args);
+                    case "whoami":
+                        return await HandleWhoAmI();
+                    case "users":
+                        return await HandleSearchUsers(args);
+                    case "accounts":
+                        return await HandleSearchAccounts(args);
+                    case "opps":
+                        return await HandleOpportunities(args);
+                    case "tasks":
+                        return await HandleTasks(args);
+                    case "query":
+                        return await HandleQuery(args);
+                    case "help":
+                    case "--help":
+                    case "-h":
+                        PrintUsage();
+                        return 0;
+                    default:
+                        Console.Error.WriteLine($"Unknown command: {command}");
+                        PrintUsage();
+                        return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error: {ex.Message}");
+                return 1;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("MSX - Microsoft CRM CLI Tool");
+            Console.WriteLine();
+            Console.WriteLine("Usage: msx <command> [arguments]");
+            Console.WriteLine();
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  auth set <cookie>                       Save your MSX authentication cookie");
+            Console.WriteLine("  auth clear                              Remove saved cookie");
+            Console.WriteLine("  auth show                               Display the saved cookie");
+            Console.WriteLine("  whoami                                  Get your system user ID");
+            Console.WriteLine("  users <name>                            Search users by name");
+            Console.WriteLine("  accounts <search>                       Search accounts by name");
+            Console.WriteLine("  opps search <account_id> <search>       Search opportunities for an account");
+            Console.WriteLine("  opps mine                               Get your associated opportunities");
+            Console.WriteLine("  opps user <user_id>                     Get opportunities for a specific user");
+            Console.WriteLine("  tasks                                   Get your recent tasks");
+            Console.WriteLine("  tasks create <title> <desc> <date>      Create a task (date: yyyy-MM-dd)");
+            Console.WriteLine("       [--account <id>]                     Tie task to an account");
+            Console.WriteLine("       [--opportunity <id>]                 Tie task to an opportunity");
+            Console.WriteLine("  query <odata_query>                     Run a raw OData query");
+        }
+
+        // ── Auth ──
+
+        private static int HandleAuth(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: msx auth set <cookie> | msx auth clear | msx auth show");
+                return 1;
+            }
+
+            string sub = args[1].ToLowerInvariant();
+
+            switch (sub)
+            {
+                case "set":
+                    if (args.Length < 3)
+                    {
+                        Console.Error.WriteLine("Usage: msx auth set <cookie>");
+                        return 1;
+                    }
+                    Directory.CreateDirectory(StorageDir);
+                    File.WriteAllText(CookiePath, args[2]);
+                    Console.WriteLine("Cookie saved.");
+                    return 0;
+
+                case "clear":
+                    if (File.Exists(CookiePath))
+                        File.Delete(CookiePath);
+                    Console.WriteLine("Cookie cleared.");
+                    return 0;
+
+                case "show":
+                    if (!File.Exists(CookiePath))
+                    {
+                        Console.Error.WriteLine("No cookie saved. Run: msx auth set <cookie>");
+                        return 1;
+                    }
+                    Console.WriteLine(File.ReadAllText(CookiePath));
+                    return 0;
+
+                default:
+                    Console.Error.WriteLine("Usage: msx auth set <cookie> | msx auth clear | msx auth show");
+                    return 1;
+            }
+        }
+
+        // ── Client helper ──
+
+        private static MsxClient GetClient()
+        {
+            if (!File.Exists(CookiePath))
+                throw new Exception("No cookie saved. Run: msx auth set <cookie>");
+            string cookie = File.ReadAllText(CookiePath).Trim();
+            return new MsxClient(cookie);
+        }
+
+        // ── WhoAmI ──
+
+        private static async Task<int> HandleWhoAmI()
+        {
+            var client = GetClient();
+            string userId = await client.WhoAmIAsync();
+            Console.WriteLine(userId);
+            return 0;
+        }
+
+        // ── Users ──
+
+        private static async Task<int> HandleSearchUsers(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: msx users <name>");
+                return 1;
+            }
+            var client = GetClient();
+            JArray users = await client.SearchUsersAsync(args[1]);
+            Console.WriteLine(users.ToString(Formatting.Indented));
+            return 0;
+        }
+
+        // ── Accounts ──
+
+        private static async Task<int> HandleSearchAccounts(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: msx accounts <search>");
+                return 1;
+            }
+            var client = GetClient();
+            JArray accounts = await client.SearchAccountsAsync(args[1]);
+            Console.WriteLine(accounts.ToString(Formatting.Indented));
+            return 0;
+        }
+
+        // ── Opportunities ──
+
+        private static async Task<int> HandleOpportunities(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: msx opps search <account_id> <search>");
+                Console.Error.WriteLine("       msx opps mine");
+                Console.Error.WriteLine("       msx opps user <user_id>");
+                return 1;
+            }
+
+            string sub = args[1].ToLowerInvariant();
+            var client = GetClient();
+
+            switch (sub)
+            {
+                case "search":
+                    if (args.Length < 4)
+                    {
+                        Console.Error.WriteLine("Usage: msx opps search <account_id> <search_term>");
+                        return 1;
+                    }
+                    JArray opps = await client.SearchOpportunitiesAsync(args[2], args[3]);
+                    Console.WriteLine(opps.ToString(Formatting.Indented));
+                    return 0;
+
+                case "mine":
+                    string myId = await client.WhoAmIAsync();
+                    JArray myOpps = await client.GetAssociatedOpportunitiesAsync(myId);
+                    Console.WriteLine(myOpps.ToString(Formatting.Indented));
+                    return 0;
+
+                case "user":
+                    if (args.Length < 3)
+                    {
+                        Console.Error.WriteLine("Usage: msx opps user <user_id>");
+                        return 1;
+                    }
+                    JArray userOpps = await client.GetAssociatedOpportunitiesAsync(args[2]);
+                    Console.WriteLine(userOpps.ToString(Formatting.Indented));
+                    return 0;
+
+                default:
+                    Console.Error.WriteLine("Unknown opps subcommand. Use: search, mine, user");
+                    return 1;
+            }
+        }
+
+        // ── Tasks ──
+
+        private static async Task<int> HandleTasks(string[] args)
+        {
+            // No subcommand = list my tasks
+            if (args.Length < 2 || args[1].ToLowerInvariant() != "create")
+            {
+                var client = GetClient();
+                JArray tasks = await client.GetMyRecentTasksAsync();
+                Console.WriteLine(tasks.ToString(Formatting.Indented));
+                return 0;
+            }
+
+            // msx tasks create <title> <description> <date> [--account <id>] [--opportunity <id>]
+            if (args.Length < 5)
+            {
+                Console.Error.WriteLine("Usage: msx tasks create <title> <description> <date> [--account <id>] [--opportunity <id>]");
+                return 1;
+            }
+
+            string title = args[2];
+            string description = args[3];
+
+            if (!DateTime.TryParse(args[4], out DateTime date))
+            {
+                Console.Error.WriteLine($"Invalid date: {args[4]}. Use a format like yyyy-MM-dd.");
+                return 1;
+            }
+
+            // Parse optional flags
+            string? accountId = null;
+            string? opportunityId = null;
+            for (int i = 5; i < args.Length - 1; i++)
+            {
+                if (args[i] == "--account")
+                    accountId = args[++i];
+                else if (args[i] == "--opportunity")
+                    opportunityId = args[++i];
+            }
+
+            var c = GetClient();
+            await c.CreateTaskAsync(title, description, date, accountId, opportunityId);
+            Console.WriteLine("Task created.");
+            return 0;
+        }
+
+        // ── Query ──
+
+        private static async Task<int> HandleQuery(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: msx query <odata_query>");
+                return 1;
+            }
+
+            // Join remaining args in case the query has spaces
+            string query = string.Join(" ", args, 1, args.Length - 1);
+            var client = GetClient();
+            JArray result = await client.RunQueryAsync(query);
+            Console.WriteLine(result.ToString(Formatting.Indented));
+            return 0;
         }
     }
 }
