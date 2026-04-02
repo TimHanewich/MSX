738fb3c88d0f7f1ee330f4849fa479390ede9418
Tim Hanewich <tahanewich@live.com>
2026-04-02 19:18:59 -0400

    Added TaskCategory enum

---
+++ src/TaskCategory.cs
@@ -0,0 +1,26 @@
+namespace MSX
+{
+    public enum TaskCategory
+    {
+        ACE = 606820000,
+        CrossSegment = 606820001,
+        CrossWorkload = 606820002,
+        PostSales = 606820003,
+        TechSupport = 606820004,
+        TechnicalCloseWinPlan = 606820005,
+        CustomerEngagement = 861980000,
+        Workshop = 861980001,
+        Demo = 861980002,
+        NegotiatePricing = 861980003,
+        ArchitectureDesignSession = 861980004,
+        PoCPilot = 861980005,
+        BlockerEscalation = 861980006,
+        ConsumptionPlan = 861980007,
+        Briefing = 861980008,
+        RFPRFI = 861980009,
+        CallBackRequested = 861980010,
+        NewPartnerRequest = 861980011,
+        Internal = 861980012,
+        ExternalCoCreationOfValue = 861980013
+    }
+}
